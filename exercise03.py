4fac970be88acf3debf5f9efc9816b5a7289c8f2
Darshan Chetan Chandavarkar <dcdarshan854@gmail.com>
2025-02-01 09:44:38 +0530

    Added exercise03.py

---
+++ exercise03.py
@@ -0,0 +1,14 @@
+#madlibs game
+#word game where you create a story by filling the blanks with random words
+
+#approach : I've used f statement
+
+adjective1 = input("Enter any adjective : ")
+noun1 = input("Enter a noun : ")
+verb1 = input("Enter a verb : ")
+adjective2 = input("enter another adjective : ")
+
+print(f"Today i went to a {adjective1} zoo")
+print(f"There i saw my friend {noun1}!")
+print(f"{noun1} was {verb1} ice-cream")
+print(f"By seeing {noun1}, i was {adjective2}")

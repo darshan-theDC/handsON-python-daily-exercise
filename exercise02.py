86e1ec08ad2fe5775f1c6266d092712ddf9bcc2f
Darshan Chetan Chandavarkar <82269548+darshan-theDC@users.noreply.github.com>
2025-02-01 10:01:31 +0530

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,8 +1,6 @@
 #shopping cart program
 #ask to enter item, price, quantity
 
-#approach : I've used input(), f statement and type coversion
-
 item = input("Enter the item you want to purchase : ")
 price = float(input("What is the price : "))
 quantity = int(input("How many would you like to buy : "))
